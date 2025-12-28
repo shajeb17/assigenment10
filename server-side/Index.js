506a04a8eedb733e57aefb865c4ed1aec9122c6c
unknown <shajeb17.11@gmail.com>
2025-12-28 18:03:43 +0600

    backend-add

--- server-side/Index.js
+++ server-side/Index.js
@@ -10,7 +10,9 @@ app.use(express.json());
 
 
 var admin = require("firebase-admin");
-var serviceAccount = require("./firebasePrivateKey.json");
+const decoded = Buffer.from(process.env.FIREBASE_SERVES_KEY, "base64").toString("utf8");
+const serviceAccount = JSON.parse(decoded);
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
